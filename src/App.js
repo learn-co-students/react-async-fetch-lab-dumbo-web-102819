3609bc938c550a2488cf0697666b6336483c045b
Dominic Chu <dominic.m.chu@gmail.com>
2020-01-23 11:12:05 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+
+import React from 'react'
+
+export default class App extends React.Component {
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(r => r.json())
+        .then((json) => {
+            console.log(json.people)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
